c861ecf0395bd5cbef7103ec72c43fce38c212cb
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-04 14:48:18 +0300

    Added files for FCcamp

---
+++ camp01.js
@@ -0,0 +1,84 @@
+'use strict';
+
+//first coment
+
+/*
+second coment
+*/
+
+// let myName;
+// let a = 7;
+
+
+// var a;
+//     a = 7;
+// var b = a; 
+// console.log(b);   
+
+// let myFirstName = 'Vitalick';
+// let myLastName = 'Bockan';
+
+// var a;
+// var b;
+// var c;
+
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+
+// a = 1 + 5;
+// b = 5 + 10;
+// c = "I am a" + " String!";
+// console.log(a, b, c);
+
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// // Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+// console.log(studlyCapVar, properCamelCase, titleCaseOver)
+
+// var catName = "Oliver";
+// var catSound = "Meow!";
+// console.log();
+
+// let catName = "Oliver";
+// let catSound = "Meow!";
+// console.log(catName, catSound);
+
+
+// const FCC = "freeCodeCamp"; // Change this line
+// let fact = "is cool!"; // Change this line
+// fact = "is awesome!";
+// console.log(FCC, fact); // Change this line
+
+
+// const myNum = 10 + 10;
+// console.log(myNum);//20
+
+// const difference = 45 - 33;
+// console.log(difference);//12
+
+// const work = 8 * 10;
+// console.log(difference);//80
+
+// const quotient = 66 / 33;
+// console.log(quotient);
+
+
+// let myVar = 87;
+// myVar++;
+// console.log(myVar);
+
+
+// let myVar = 11;
+//     myVar--;
+//     console.log(myVar);
+
+// const myDecimal = 5.7;
+
+const product = 2.0 * 2.5;
+console.log(product);//5
