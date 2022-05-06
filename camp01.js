4b12aafafd8ccd90fd73cf55ee1ca8cb85cb5898
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-06 15:41:41 +0300

    Ordered List

--- camp01.js
+++ camp01.js
@@ -149,6 +149,6 @@ second coment
 // const myStr = "This is the start. " + "This is the end.";
 // console.log(myStr);
 
-let myStr = "This is the first sentence. ";
-myStr += "This is the second sentence.";
-console.log(myStr);
+// let myStr = "This is the first sentence. ";
+// myStr += "This is the second sentence.";
+// console.log(myStr);
