11b31a5e4b20f4c12a1f818c887f3447f4fe6945
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-08 14:33:05 +0300

    Access Array Data with Indexes

--- array02.js
+++ array02.js
@@ -3,5 +3,9 @@
 //Arrays
 // const myArray = ['string', 8];
 
-const myArray = [['sting', 8], [1, 2, 3], [{}, 88]];
+// const myArray = [['sting', 8], [1, 2, 3], [{}, 88]];
+
+const myArray = [50, 60, 70];
+let myData = myArray[0];
+console.log(myData);
 
