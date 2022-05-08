566b5ddf813a40158abbe398c5081e3bb0030593
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-08 13:50:23 +0300

    array

--- camp01.js
+++ camp01.js
@@ -188,9 +188,12 @@ second coment
 // const secondToLastLetterOfLastName = lastName[lastName.length -2];
 // console.log(secondToLastLetterOfLastName);
 
-const myNoun = "dog";
-const myAdjective = "big";
-const myVerb = "ran";
-const myAdverb = "quickly";
-const wordBlanks = "The " + myNoun + "" + myAdjective + "" + myVerb + "" + myAdverb + "."; 
-console.log(wordBlanks);
+// const myNoun = "dog";
+// const myAdjective = "big";
+// const myVerb = "ran";
+// const myAdverb = "quickly";
+// const wordBlanks = "The " + myNoun + "" + myAdjective + "" + myVerb + "" + myAdverb + "."; 
+// console.log(wordBlanks);
+
+const myArray = ['string', 8];
+
