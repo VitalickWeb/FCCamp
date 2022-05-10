c11053d0685c1683e1b8b2e5b78b8d6dab1ef5dc
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-10 13:42:13 +0300

    Return a Value from a Function

--- function03.js
+++ function03.js
@@ -5,8 +5,15 @@
 // }
 // reusableFunction();
 
-function functionWithArgs(param1, param2) {
-    console.log(param1 + param2);
+// function functionWithArgs(param1, param2) {
+//     console.log(param1 + param2);
+// }
+// functionWithArgs(1, 2);
+// functionWithArgs(7, 9);
+
+function timesFive(num) {
+    return num * 5;
 }
-functionWithArgs(1, 2);
-functionWithArgs(7, 9);
+console.log(timesFive(5));
+console.log(timesFive(2));
+console.log(timesFive(0));
