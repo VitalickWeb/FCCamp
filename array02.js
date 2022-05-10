1ca8519d8b0da6a58dff4941a0c403e1aebca7a5
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-10 11:36:27 +0300

    Recordingoverwrite the array

--- array02.js
+++ array02.js
@@ -5,7 +5,15 @@
 
 // const myArray = [['sting', 8], [1, 2, 3], [{}, 88]];
 
-const myArray = [50, 60, 70];
-let myData = myArray[0];
-console.log(myData);
+// const myArray = [50, 60, 70];
+// let myData = myArray[0];
+// console.log(myData);
+
+const myArray = [18, 64, 99];
+myArray[0] = 45;
+console.log(myArray);
+
+   
+
+
 
