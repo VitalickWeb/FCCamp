32532cafe3c140989c747cc32f92ecca28ff137a
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-10 14:49:07 +0300

    Global Scope and Functions

--- function03.js
+++ function03.js
@@ -11,9 +11,29 @@
 // functionWithArgs(1, 2);
 // functionWithArgs(7, 9);
 
-function timesFive(num) {
-    return num * 5;
+// function timesFive(num) {
+//     return num * 5;
+// }
+// console.log(timesFive(5));
+// console.log(timesFive(2));
+// console.log(timesFive(0));
+
+//Global Scope and Functions
+let oopsGlobal; 
+function fun1() {
+   oopsGlobal = 5;
 }
-console.log(timesFive(5));
-console.log(timesFive(2));
-console.log(timesFive(0));
+ 
+let myGlobal = 10;
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+fun1();
+fun2();
