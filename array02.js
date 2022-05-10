7f6ea694a1b110eab4be77d38e4d4483d33eb35c
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-10 12:16:16 +0300

    Manipulate Arrays With push()

--- array02.js
+++ array02.js
@@ -13,15 +13,16 @@
 // myArray[0] = 45;
 // console.log(myArray);
 
-const myArray = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [[10, 11, 12], 13, 14],
-  ];
+// const myArray = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [[10, 11, 12], 13, 14],
+//   ];
   
-const myData = myArray[2][1];
-console.log(myData);
+// const myData = myArray[2][1];
+// console.log(myData);
 
-
-
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+console.log(myArray);
