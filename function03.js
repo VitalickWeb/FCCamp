6f2d1def8a2be37b136cef8edd618580ae7653d0
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-12 17:36:47 +0300

    Comparison with the Inequality Operator

--- function03.js
+++ function03.js
@@ -130,3 +130,13 @@
 //   }
   
 //   compareEquality(10, "10");
+
+
+function testNotEqual(val) {
+    if (val != 99) { 
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
