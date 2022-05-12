f1ca36d5a3db47b4335fedb2964d0b97b5e002e3
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-12 17:20:30 +0300

    Practice comparing different values

--- function03.js
+++ function03.js
@@ -121,3 +121,12 @@
 //   }
   
 //   console.log(testStrict(10));
+
+function compareEquality(a, b) {
+    if (a === b) { 
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
