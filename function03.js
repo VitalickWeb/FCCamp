b446c2a7900fd993d32b662c289c61740bf3ab88
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-13 17:06:20 +0300

    Comparison with the Strict Inequality Operator

--- function03.js
+++ function03.js
@@ -132,11 +132,21 @@
 //   compareEquality(10, "10");
 
 
-function testNotEqual(val) {
-    if (val != 99) { 
+// function testNotEqual(val) {
+//     if (val != 99) { 
+//       return "Not Equal";
+//     }
+//     return "Equal";
+//   }
+  
+//   testNotEqual(10);
+
+
+function testStrictNotEqual(val) {
+    if (val !== 17) {
       return "Not Equal";
     }
     return "Equal";
   }
   
-  testNotEqual(10);
+  testStrictNotEqual(10);
