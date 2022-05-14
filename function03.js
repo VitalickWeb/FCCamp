80ddb745aac4a9c9c37fae01a39cf91115106936
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-14 17:38:12 +0300

    Introducing Else If Statements

--- function03.js
+++ function03.js
@@ -229,16 +229,30 @@
 //   testLogicalOr(15);
 
 
-function testElse(val) {
-    let result = "";
+// function testElse(val) {
+//     let result = "";
    
-    if (val > 5) {
-        result = "Bigger than 5";
-    } else {
-        result = "5 or Smaller";
-    }
+//     if (val > 5) {
+//         result = "Bigger than 5";
+//     } else {
+//         result = "5 or Smaller";
+//     }
   
-    return result;
+//     return result;
+//   }
+  
+//   testElse(4);
+
+
+
+function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    } else if (val < 5) {
+      return "Smaller than 5";
+    } else {
+      return "Between 5 and 10";
+    }
   }
   
-  testElse(4);
+  testElseIf(7);
