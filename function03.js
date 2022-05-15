1dd45b68d806a1ab37fe4dea6e78d3974a29c52f
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-15 11:56:19 +0300

    Logical Order in If Else Statements

--- function03.js
+++ function03.js
@@ -245,14 +245,27 @@
 
 
 
-function testElseIf(val) {
-    if (val > 10) {
-      return "Greater than 10";
-    } else if (val < 5) {
-      return "Smaller than 5";
+// function testElseIf(val) {
+//     if (val > 10) {
+//       return "Greater than 10";
+//     } else if (val < 5) {
+//       return "Smaller than 5";
+//     } else {
+//       return "Between 5 and 10";
+//     }
+//   }
+  
+//   testElseIf(7);
+
+
+function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
     } else {
-      return "Between 5 and 10";
+      return "Greater than or equal to 10";
     }
   }
   
-  testElseIf(7);
+  console.log(orderMyLogic(7));
