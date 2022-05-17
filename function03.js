a2e77b827464e4ad7ab3cbc0f35abeb16df98fe4
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-17 16:03:17 +0300

    Selecting from Many Options with Switch Statements

--- function03.js
+++ function03.js
@@ -290,26 +290,48 @@
 //   testSize(7);
 
 
-const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+// const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 
-function golfScore(par, strokes) {
-    if (strokes == 1) {
-        return names[0];
-    } else if (strokes <= par - 2) {
-        return names[1];
-    } else if (strokes == par - 1) {
-        return names[2];
-    } else if (strokes == par) {
-        return names[3];
-    } else if (strokes == par + 1) {
-        return names[4];
-    } else if (strokes == par + 2) {
-        return names[5];
-    } else if (strokes >= par + 3) {
-        return names[6];
-    }
+// function golfScore(par, strokes) {
+//     if (strokes == 1) {
+//         return names[0];
+//     } else if (strokes <= par - 2) {
+//         return names[1];
+//     } else if (strokes == par - 1) {
+//         return names[2];
+//     } else if (strokes == par) {
+//         return names[3];
+//     } else if (strokes == par + 1) {
+//         return names[4];
+//     } else if (strokes == par + 2) {
+//         return names[5];
+//     } else if (strokes >= par + 3) {
+//         return names[6];
+//     }
 
-  return "Change Me";
+//   return "Change Me";
+// }
+
+// golfScore(5, 4);
+
+
+function caseInSwitch(val) {
+    let answer = "";
+    switch(val) {
+        case 1:
+            answer = 'alpha';
+        break;
+        case 2:
+            answer = 'beta';
+        break;
+        case 3:
+            answer = 'gamma';
+        break;
+        case 4:
+            answer = 'delta';
+        break;
+    }      
+        return answer;
+    
 }
-
-golfScore(5, 4);
+  console.log(caseInSwitch(1));
