8bcf6781ce215fc3eb93528c93ccdf47938f6c9e
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-17 15:10:05 +0300

    Chaining If Else Statements

--- function03.js
+++ function03.js
@@ -258,14 +258,33 @@
 //   testElseIf(7);
 
 
-function orderMyLogic(val) {
-    if (val < 5) {
-      return "Less than 5";
-    } else if (val < 10) {
-      return "Less than 10";
-    } else {
-      return "Greater than or equal to 10";
+// function orderMyLogic(val) {
+//     if (val < 5) {
+//       return "Less than 5";
+//     } else if (val < 10) {
+//       return "Less than 10";
+//     } else {
+//       return "Greater than or equal to 10";
+//     }
+//   }
+  
+//   console.log(orderMyLogic(7));
+
+
+function testSize(num) {
+    if (num < 5) {
+        return 'Tiny';
+    } else if (num < 10) {
+        return 'Small';
+    } else if (num < 15) {
+        return 'Medium';
+    } else if (num < 20) {
+        return 'Large';
+    } else if (num <= 25) {
+        return 'Huge';
     }
+
+    return "Change Me";
   }
   
-  console.log(orderMyLogic(7));
+  testSize(7);
