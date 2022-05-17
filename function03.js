326cc2caf4fab0840769c1092d7e005778268973
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-17 15:36:31 +0300

    Golf Code

--- function03.js
+++ function03.js
@@ -271,20 +271,45 @@
 //   console.log(orderMyLogic(7));
 
 
-function testSize(num) {
-    if (num < 5) {
-        return 'Tiny';
-    } else if (num < 10) {
-        return 'Small';
-    } else if (num < 15) {
-        return 'Medium';
-    } else if (num < 20) {
-        return 'Large';
-    } else if (num <= 25) {
-        return 'Huge';
+// function testSize(num) {
+//     if (num < 5) {
+//         return 'Tiny';
+//     } else if (num < 10) {
+//         return 'Small';
+//     } else if (num < 15) {
+//         return 'Medium';
+//     } else if (num < 20) {
+//         return 'Large';
+//     } else if (num <= 25) {
+//         return 'Huge';
+//     }
+
+//     return "Change Me";
+//   }
+  
+//   testSize(7);
+
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+    if (strokes == 1) {
+        return names[0];
+    } else if (strokes <= par - 2) {
+        return names[1];
+    } else if (strokes == par - 1) {
+        return names[2];
+    } else if (strokes == par) {
+        return names[3];
+    } else if (strokes == par + 1) {
+        return names[4];
+    } else if (strokes == par + 2) {
+        return names[5];
+    } else if (strokes >= par + 3) {
+        return names[6];
     }
 
-    return "Change Me";
-  }
-  
-  testSize(7);
+  return "Change Me";
+}
+
+golfScore(5, 4);
